a6ab738c3a2188a18add6b82f422ab6c38ee53e7
mon00kus <raulmoncadac@gmail.com>
2024-01-29 17:16:48 +0100

    Navegacion inicial

---
+++ client/src/app/app.routes.ts
@@ -0,0 +1,14 @@
+import { Routes } from '@angular/router';
+import { GameListComponent } from './components/game-list/game-list.component';
+
+export const routes: Routes = [
+    {
+        path:'',
+        redirectTo:'/games',
+        pathMatch:'full'
+    },
+    {
+        path:'games',
+        component: GameListComponent
+    }
+];
